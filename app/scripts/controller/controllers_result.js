c064e33fc74efee18e574d721274251834254083
Marie-Elise Lecoq <MEGAUTIER@Marie-Elises-MacBook-Air.local>
2013-11-24 18:54:47 +0100

    derniere changement

--- app/scripts/controller/controllers_result.js
+++ app/scripts/controller/controllers_result.js
@@ -2,10 +2,28 @@
 
 /* Controllers */
 
-function CtrlResult($scope){
+function CtrlResult($scope, searchForm, $http){
+	$scope.scientificNames=searchForm.getScientificName();
+  	$scope.vernacularNames=searchForm.getVernacularName();
+  	$scope.localities=searchForm.getLocality();
+  	$scope.latitudes=searchForm.getLatitude();
+  	$scope.longitudes=searchForm.getLongitude();
+  	$scope.boundingBoxes=searchForm.getBoundingBoxes();
+  	$scope.datapublishers=searchForm.getDatapublisher();
+  	$scope.datasets=searchForm.getDataset();
+  	$scope.dates=searchForm.getDate();
+  	$scope.georeferences = searchForm.getGeoreferencedData();
+  	$scope.datapublisherDataset = searchForm.getDatapublisherDataset();
 
+	$scope.json = searchForm.buildJson();
 
+	$http.post("http://localhost:9000/", $scope.json)
+		.success(function(data, status) {
+      		$scope.reponse = status;
+    	})
+    	.error(function(data, status) {
+      		$scope.reponse = status;
+    	});
 }
 
-
-myApp.controller('CtrlResult', ['$scope', CtrlResult]);
+myApp.controller('CtrlResult', ['$scope', 'searchForm', '$http', CtrlResult]);
