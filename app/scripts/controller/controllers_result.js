70226fa7a745d4969a4ce6450c033080b7bf8833
Marie-Elise Lecoq <MEGAUTIER@Marie-Elises-MacBook-Air.local>
2013-11-28 11:11:46 +0100

    modif client dataset/datapublisher

--- app/scripts/controller/controllers_result.js
+++ app/scripts/controller/controllers_result.js
@@ -3,20 +3,19 @@
 /* Controllers */
 
 function CtrlResult($scope, searchForm, $http){
-	$scope.scientificNames=searchForm.getScientificName();
-  	$scope.vernacularNames=searchForm.getVernacularName();
-  	$scope.localities=searchForm.getLocality();
-  	$scope.latitudes=searchForm.getLatitude();
-  	$scope.longitudes=searchForm.getLongitude();
-  	$scope.boundingBoxes=searchForm.getBoundingBoxes();
-  	$scope.datapublishers=searchForm.getDatapublisher();
-  	$scope.datasets=searchForm.getDataset();
-  	$scope.dates=searchForm.getDate();
-  	$scope.georeferences = searchForm.getGeoreferencedData();
-  	$scope.datapublisherDataset = searchForm.getDatapublisherDataset();
-    $scope.isCollapsedRecherche = true;
-
-	$scope.json = searchForm.buildJson();
+  $scope.scientificNames=searchForm.getScientificName();
+  $scope.vernacularNames=searchForm.getVernacularName();
+  $scope.localities=searchForm.getLocality();
+  $scope.latitudes=searchForm.getLatitude();
+  $scope.longitudes=searchForm.getLongitude();
+  $scope.boundingBoxes=searchForm.getBoundingBoxes();
+  $scope.datapublishers=searchForm.getDatapublisher();
+  $scope.datasets=searchForm.getDataset();
+  $scope.dates=searchForm.getDate();
+	$scope.georeferences = searchForm.getGeoreferencedData();
+  $scope.datapublisherDataset = searchForm.getDatapublisherDataset();
+  $scope.isCollapsedRecherche = true;
+  $scope.json = searchForm.buildJson();
 
 	$http.post("http://localhost:9000/api/search ", $scope.json)
 		.success(function(data, status) {
