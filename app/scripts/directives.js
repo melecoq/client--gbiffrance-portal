ba4d850288ef1358277491b3bbd8e184f2c9e1cd
Marie-Elise Lecoq <MEGAUTIER@Marie-Elises-MacBook-Air.local>
2013-12-04 15:13:32 +0100

    diver modif

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -92,3 +92,21 @@ var dateSlider = function() {
 		}
 	};
 };
+
+var barsChart = function($parse){
+	var directeDefinitionObject = {
+		restrict:'E',
+		replace:false,
+		scope: {data: '=charData'},
+		link: function (scope, element, attrs){
+			var chart = d3.select(element[0]);
+			chart.append("div").attr("class", "chart")
+             .selectAll('div')
+             .data(scope.data).enter().append("div")
+             .transition().ease("elastic")
+             .style("width", function(d) { return d + "%"; })
+             .text(function(d) { return d + "%"; });
+		}
+	};
+	return directeDefinitionObject;
+};
